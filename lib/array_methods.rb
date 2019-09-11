9170916d8b0c968a3e3f48969d442ab0414341f7
James Schaffer <jas.schaffer@gmail.com>
2019-09-11 05:07:22 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -28,7 +28,7 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
- max_value = array[0]
+  max_value = array[0]
   counter = 1
   while array[counter] do
     if array[counter] < max_value 
